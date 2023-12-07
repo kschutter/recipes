8a41b41537bddd065080bdf35efba79487315539
Kyle Schutter <kmosswork@gmail.com>
2023-12-07 11:34:53 -0700

    Fixed "and chicken" links

--- src/config.py
+++ src/config.py
@@ -23,17 +23,18 @@ def get_recipes():
             continue
         readme += f"\n## {category}"
 
+        edited_titles = {}
         recipe_list = os.listdir("r/" + category)
         for idx, recipe_file in enumerate(recipe_list):
-            if category == 'Poultry' and recipe_file.startswith('Chicken_and'):
-                continue
-            if category == 'Poultry' and recipe_file.startswith('Chicken_'):
-                recipe_file = recipe_file[8:-3] + ',_Chicken' + recipe_file[-3:]
-                recipe_list[idx] = recipe_file
+            if category == 'Poultry' and recipe_file.startswith('Chicken_') and not recipe_file.startswith('Chicken_and'):
+                edited_titles[recipe_file] = recipe_file[8:-3] + ',_Chicken' + recipe_file[-3:]
 
         for recipe_file in sorted(recipe_list):
             link = f"r/{category}/{recipe_file}"
-            recipe_name = recipe_file[:-3].replace("_", " ")
+            if recipe_file in edited_titles:
+                recipe_name = edited_titles[recipe_file].replace("_", " ")
+            else:
+                recipe_name = recipe_file[:-3].replace("_", " ")
             if recipe_name[:11] == "Slow-Cooker":
                 slow_cooker.append(
                     {"title": f"{recipe_name[12:]} ({emojis[category]})", "link": link}
